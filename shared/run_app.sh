ffc63a63b349e662107af449c43aaf8a9efdafa8
Juan Leni <lenijuan@gmail.com>
2021-11-28 22:55:58 +0100

    fix incorrect binary name

--- shared/run_app.sh
+++ shared/run_app.sh
@@ -3,7 +3,7 @@
 DIR=`dirname $0`
 
 ROOTFS="${DIR}/zondbox-distro/build/tmp/work/qemu_optee32-poky-linux-gnueabi/core-image-minimal/1.0-r0/rootfs/"
-RUSTEE_HOST=${ROOTFS}/usr/bin/hello_rustee
+RUSTEE_HOST=${ROOTFS}/usr/bin/hello-rustee
 
 echo ------------------------ Installing TAs -------------------------------
 cp $ROOTFS/lib/optee_armtz/*.ta /lib/optee_armtz/
