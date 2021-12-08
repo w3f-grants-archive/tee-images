5e9dd312920e2d58cc797a140b5889ce08ec9b8f
Natanael <neithanmo@gmail.com>
2021-12-08 11:09:48 -0600

    fix(workspace): This updates the path to TA and Host

--- shared/run_app.sh
+++ shared/run_app.sh
@@ -2,17 +2,18 @@
 # ATTENTION: THIS SCRIPT IS SUPPOSED TO RUN INSIDE QEMU!!
 DIR=`dirname $0`
 
-ROOTFS="${DIR}/zondbox-distro/build/tmp/work/qemu_optee32-poky-linux-gnueabi/core-image-minimal/1.0-r0/rootfs/"
+ROOTFS="${DIR}/zondbox-distro/build/tmp/work/qemu_optee32-poky-linux-gnueabi/hello-rustee/0.1-r0/image"
 RUSTEE_HOST=${ROOTFS}/usr/bin/hello-rustee
+RUSTEE_TA=${ROOTFS}/lib/optee_armtz/*.ta
 
 echo ------------------------ Installing TAs -------------------------------
-cp $ROOTFS/lib/optee_armtz/*.ta /lib/optee_armtz/
+cp $RUSTEE_TA /lib/optee_armtz/
 
 echo ------------------------ Launching HOST -------------------------------
 echo
 
 if test -f "$RUSTEE_HOST"; then
-	$RUSTEE_HOST
+	RUST_LOG=debug $RUSTEE_HOST
 else
 	echo "File binary does not exist. Did you compile your app?"
 fi
